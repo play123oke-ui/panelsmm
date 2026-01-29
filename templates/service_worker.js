4627221de08c3c23a3691791553e8e1d707a168c
Black White <play123oke@gmail.com>
2026-01-30 03:10:41 +0700

    Create service_worker.js

---
+++ templates/service_worker.js
@@ -0,0 +1,11 @@
+<script>
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(registration => {
+        console.log('Service worker registered:', registration);
+      })
+      .catch(error => {
+        console.error('Service worker registration failed:', error);
+      });
+  }
+</script>
