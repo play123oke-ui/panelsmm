d95b53481b09a093f0405b51d099328f49c34630
Black White <play123oke@gmail.com>
2026-01-29 16:39:11 +0700

    Update panel.py

--- panel.py
+++ panel.py
@@ -101,4 +101,4 @@ def payments():
     return render_template('payments.html', payments=payments)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    app.run(debug=True, port=5000)
