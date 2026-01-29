3a48d4e7362fc0ea36378879a7c8f12ec8135699
Black White <play123oke@gmail.com>
2026-01-29 22:11:07 +0700

    Update psmm.py

--- psmm.py
+++ psmm.py
@@ -26,7 +26,7 @@ db = SQLAlchemy(app)
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlalchemy://panelsmm.db>
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlalchemy://panelsmm.db'
 app.config['SECRET_KEY'] = api_key
 db = SQLAlchemy(app)
 
