7695a8f81dbc6e19c8716cd58bbd815d73f909fa
Black White <play123oke@gmail.com>
2026-01-31 23:45:24 +0700

    Update psmm.py

--- psmm.py
+++ psmm.py
@@ -11,7 +11,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 app = Flask(__name__, template_folder='templates')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///panelsmm.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-api_key = secrets.token_urlsafe(32)
+api_key = secrets.api_key
 app.config['SECRET_KEY'] = api_key
 db = SQLAlchemy(app)
 
